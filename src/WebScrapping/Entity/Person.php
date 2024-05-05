58246b02f2baedc6d3d8a8835bf64c98e6f1e991
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2024-05-05 15:36:54 -0300

    fix: ajustes na Entity adicionar métodos get

---
+++ src/WebScrapping/Entity/Person.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Chuva\Php\WebScrapping\Entity;
+
+/**
+ * Paper Author personal information.
+ */
+class Person
+{
+
+    /**
+     * Person name.
+     */
+    public string $name;
+
+    /**
+     * Person institution.
+     */
+    public string $institution;
+
+    /**
+     * Builder.
+     */
+    public function __construct($name, $institution)
+    {
+        $this->name = $name;
+        $this->institution = $institution;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInstitution(): string
+    {
+        return $this->institution;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+}
